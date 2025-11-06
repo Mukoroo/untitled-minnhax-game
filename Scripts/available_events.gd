cbf4341c5a1e9e21c0037ac148cd120164c3750f
Mukoroo <mukorplays@gmail.com>
2025-11-06 21:07:02 +0000

    0.010 extra events
    Add events in available events list

--- Scripts/available_events.gd
+++ Scripts/available_events.gd
@@ -5,8 +5,9 @@ var event1 = {"name":"WorkLate", "type":"generic", "description":"I have to work
 var event2 = {"name":"Vacation", "type":"personal", "description":"My friends want me to come on vacation with them", "choice1":"Have a great break!", "choice2":"No way, I'll take you somewhere!"}
 var event3 = {"name":"Pet", "type":"generic", "description":"I want a pet!", "choice1":"That'll be so cute!", "choice2":"That'll be so much work!"}
 var event4 = {"name":"SephirothLove", "type":"personal", "description":"Do you still love me?", "choice1":"Your mommy issues are too much", "choice2":"Let's merge with the lifestream...together!"}
-var event5 = {"name":"Pet", "type":"generic", "description":"I want a pet!", "choice1":"That'll be so cute!", "choice2":"That'll be so much work!"}
-var event6 = {"name":"Pet", "type":"generic", "description":"I want a pet!", "choice1":"That'll be so cute!", "choice2":"That'll be so much work!"}
-var event7 = {"name":"Pet", "type":"generic", "description":"I want a pet!", "choice1":"That'll be so cute!", "choice2":"That'll be so much work!"}
+var event5 = {"name":"Wedding+1", "type":"generic", "description":"Will you be my +1 to my cousin's wedding?", "choice1":"That sounds wonderful!", "choice2":"That sounds like a living hell!"}
+var event6 = {"name":"Parents", "type":"generic", "description":"My parents want to meet you", "choice1":"Let's go to dinner", "choice2":"Let's go do karaoke"}
+var event7 = {"name":"RobinHood", "type":"generic", "description":"I just stole Elon Musk's bitcoin wallet", "choice1":"Let's redistribute that wealth, comrade!", "choice2":"Hello, officer? Yes, this one right here!"}
+var event8 = {"name":"Gambling", "type":"generic", "description":"I've started playing high stakes poker and I feel like I have to keep chasing those wins", "choice1":"All in! I'll start shopping for five star hotel breaks!", "choice2":"Maybe you should ease off if it stops being fun"}
 
-var availableEvents = [event0, event1, event2, event3]
+var availableEvents = [event0, event1, event2, event3, event4, event5, event6, event7, event8]
